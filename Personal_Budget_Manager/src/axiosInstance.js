338f2d46ec473fe71f61c73df8f9c270bf5e880f
Jashwanth-05 <jashwanthj510@gmail.com>
2025-11-04 11:42:12 +0530

    Axios Change

--- Personal_Budget_Manager/src/axiosInstance.js
+++ Personal_Budget_Manager/src/axiosInstance.js
@@ -2,12 +2,17 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://personal-budget-manager-backend.onrender.com",
+  withCredentials: true,
 });
 
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  // It's also good practice to ensure the Content-Type is set, though Axios often handles this.
+  req.headers['Content-Type'] = 'application/json';
   return req;
 });
 
